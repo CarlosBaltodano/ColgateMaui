c5a8a56dd7d19b1dd519896f6c90b3562de97371
User <carlosquispe9876@gmail.com>
2023-09-12 17:07:30 -0500

    login

---
+++ Xplora/ViewModel/MainVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Xplora.ViewModel
+{
+    public class MainVM :BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+        public MainVM(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
